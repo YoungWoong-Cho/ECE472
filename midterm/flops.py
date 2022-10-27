81f065f56aa7b6340e1601414c17ba099bb9ac19
Youngwoong Cho <youngwoongcho@Youngwoongs-MacBook-Pro.local>
2022-10-26 21:29:27 -0400

    update ViT

--- midterm/flops.py
+++ midterm/flops.py
@@ -7,7 +7,8 @@ from model.PiT import PiT
 import torch
 
 if __name__=='__main__':
-    # PiT_S16 = PiT(iimg_size=CONFIG['model']['img_size'],
+    # PiT_B16 = PiT(img_size=CONFIG['model']['img_size'], num_classes=100)
+    # PiT_S16 = PiT(img_size=CONFIG['model']['img_size'],
     #               num_classes=100,
     #               patch_size=16,
     #               stride=8,
@@ -15,6 +16,14 @@ if __name__=='__main__':
     #               depth=[2, 6, 4],
     #               heads=[3, 6, 12],
     #               mlp_ratio=4,)
+    # PiT_Ti16 = PiT(img_size=CONFIG['model']['img_size'],
+    #                 num_classes=100,
+    #                 patch_size=16,
+    #                 stride=8,
+    #                 base_dims=[32, 32, 32],
+    #                 depth=[2, 6, 4],
+    #                 heads=[2, 4, 8],
+    #                 mlp_ratio=4,)
 
     # ViT_B16 = ViT('B_16', image_size=CONFIG['model']['img_size'],
     #               num_classes=100)
@@ -22,7 +31,12 @@ if __name__=='__main__':
     #               num_classes=100,
     #               dim=384,
     #               num_layers=12, num_heads=6)
-    model = PiT(img_size=CONFIG['model']['img_size'], num_classes=100)
-    input = torch.rand([1, CONFIG['train']['batch_size']], CONFIG['model']['img_size'], CONFIG['model']['img_size'])
+    ViT_Ti16 = ViT('B_16', image_size=CONFIG['model']['img_size'], num_classes=100,
+                  dim=192, num_layers=12, num_heads=3)
+
+    model = ViT_Ti16
+    model.eval()
+    # torch.Size([512, 3, 112, 112])
+    input = torch.rand([1, 3, CONFIG['model']['img_size'], CONFIG['model']['img_size']])
     
     compute_flops(model, input)
--- midterm/utils/plot.py
+++ midterm/utils/plot.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 
-# B, S, XS, Ti
+# B, S, Ti
 data = {
     "ViT": {
-        "GFLOPS": [],
+        "GFLOPS": [4.326590976, 4.326590976, 4.326590976],
         "train_loss": [],
         "train_accuracy": [0.39452, 0.39894],
         "eval_accuracy": [0.3253, 0.3327]
     },
     "PiT": {
-        "GFLOPS": [],
+        "GFLOPS": [2.761954304, 0.701461728, 0.150245248],
         "train_loss": [],
         "train_accuracy": [0.54568, 0.4706],
         "eval_accuracy": [0.4244, 0.3981]
